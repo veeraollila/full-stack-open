0474ca63e002c06da46496cc0be621c8708206ee
veeraollila <veera.ollila@live.fi>
2024-11-09 14:28:52 +0200

    2c examples

--- osa2/examples/src/App.js
+++ osa2/examples/src/App.js
@@ -1,21 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(false)
 
-  const [newNote, setNewNote] = useState(
-    'a new note...'
-  )
-
-  const [showAll, setShowAll] = useState(true)
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        setNotes(response.data)
+      })
+  }, [])
 
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
       important: Math.random() > 0.5,
-      id: String(notes.length + 1),
+      id: notes.length + 1,
     }
   
     setNotes(notes.concat(noteObject))
@@ -23,13 +28,12 @@ const App = (props) => {
   }
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
     setNewNote(event.target.value)
   }
 
   const notesToShow = showAll
     ? notes
-    : notes.filter(note => note.important === true)
+    : notes.filter(note => note.important)
 
   return (
     <div>
@@ -38,19 +42,19 @@ const App = (props) => {
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
         </button>
-      </div>   
+      </div>      
       <ul>
-        {notesToShow.map(note =>
+        {notesToShow.map(note => 
           <Note key={note.id} note={note} />
         )}
       </ul>
       <form onSubmit={addNote}>
-        <input 
-          value={newNote} 
-          onChange={handleNoteChange} 
+      <input
+          value={newNote}
+          onChange={handleNoteChange}
         />
         <button type="submit">save</button>
-      </form>
+      </form> 
     </div>
   )
 }
