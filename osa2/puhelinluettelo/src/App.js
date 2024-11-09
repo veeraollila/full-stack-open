50313efd26b8ce61ad428b9970381a6da5f5be91
veeraollila <veera.ollila@live.fi>
2024-11-09 13:15:48 +0200

    puhelinluettelo step4

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,19 +3,23 @@ import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const addPerson = (event) =>{
     event.preventDefault()
     const newPerson = {name: newName, number: newNumber}
-   
     if(persons.find((person) => person.name.toLowerCase() === newName.toLowerCase()))
       alert(`Person ${newName} already exists!`)
-    
+    else if(persons.find((person) => person.number === newNumber))
+      alert(`Number ${newNumber} already exists!`)
     else
       setPersons(persons.concat(newPerson))
     setNewName('')
@@ -30,9 +34,19 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handeFilterInput = (event) =>{
+    setFilter(event.target.value)
+  }
+
+  const filteredNames = persons.filter(persons => persons.name.toLowerCase().includes(filter.toLowerCase()))
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Search name: <input value={filter} onChange={handeFilterInput}/>
+      </div>
+      <h2>Add Person</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameInput}/>
@@ -46,7 +60,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => 
+        {filteredNames.map(person => 
           <Person key={person.name} person={person} />
         )}
       </ul>
