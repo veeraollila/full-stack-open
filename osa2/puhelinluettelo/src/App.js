eec3f1f6da4c58b0c1a013a5bb4673557aaba827
veeraollila <veera.ollila@live.fi>
2024-11-09 13:13:29 +0200

    puhelinluettelo step3

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -5,20 +5,29 @@ const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
+
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) =>{
     event.preventDefault()
-    const newPerson = {name: newName}
-    if(persons.find((person) => person.name === newName))
-      alert(`${newName} already exists!`)
+    const newPerson = {name: newName, number: newNumber}
+   
+    if(persons.find((person) => person.name.toLowerCase() === newName.toLowerCase()))
+      alert(`Person ${newName} already exists!`)
+    
     else
       setPersons(persons.concat(newPerson))
     setNewName('')
+    setNewNumber('')
   }
 
-  const handleNameInput = (event)=>{
+  const handleNameInput = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handlePhoneInput = (event) =>{
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -27,6 +36,9 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameInput}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handlePhoneInput}/>
         </div>
         <div>
           <button type="submit">add</button>
